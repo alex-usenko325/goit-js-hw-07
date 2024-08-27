e6d6f559a99377acd766399aa8a52e86ead1c7f5
Zergus325 <alex.usenko.97@gmail.com>
2024-08-27 19:11:07 +0300

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = form.elements.email.value.trim();
+  const passworld = form.elements.passworld.value.trim();
+
+  if (email === "" || passworld === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email,
+    passworld,
+  };
+
+  console.log(formData);
+
+  form.reset();
+});
