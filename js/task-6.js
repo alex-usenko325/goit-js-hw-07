01b1a2199a3f1df74106ef86e3cfa12da67655d7
Zergus325 <alex.usenko.97@gmail.com>
2024-08-27 21:09:03 +0300

    task6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("#controls input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+createButton.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+
+  if (amount < 1 || amount > 100 || isNaN(amount)) {
+    alert("Please enter a number between 1 and 100.");
+    return;
+  }
+
+  createBoxes(amount);
+  input.value = "";
+});
+
+function createBoxes(amount) {
+  boxesContainer.innerHTML = "";
+  const fragment = document.createDocumentFragment();
+  let boxSize = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = "5px";
+
+    fragment.appendChild(box);
+    boxSize += 10;
+  }
+  boxesContainer.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
