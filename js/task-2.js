b8961fe861edfa1834f91b4748cf727223648e25
Zergus325 <alex.usenko.97@gmail.com>
2024-08-27 18:27:20 +0300

    task2.0

--- js/task-2.js
+++ js/task-2.js
@@ -25,10 +25,12 @@ const images = [
   },
 ];
 
-const gallery = (document = images
-  .map((images) => {
+const gallery = document.querySelector(".gallery");
+
+const galleryItems = images
+  .map((image) => {
     return `<li><img src="${image.url}" alt="${image.alt}" /></li>`;
   })
-  .join(""));
+  .join("");
 
 gallery.insertAdjacentHTML("beforeend", galleryItems);
